6cb2d5cb9c7f879a12e66777e1d108c8e1e56e3d
Alex Klm <alexcortez333@gmail.com>
2018-12-04 21:36:48 +0200

    add babelrc

--- MasterPizza/src/js/index.js
+++ MasterPizza/src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import num from './test';
-
-console.log(`I imported ${num} from another module! Some test text`);
+const x = 23;
+console.log(`I imported ${num} from another module! Some test text. varibles x is ${x}`);
